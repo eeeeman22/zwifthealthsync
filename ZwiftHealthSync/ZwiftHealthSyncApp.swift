961c90eda87e4c300827b39235d4cb55908e81ec
eeeeman22 <eeeeman@icloud.com>
2026-01-31 15:54:12 -0500

    Initial Commit

---
+++ ZwiftHealthSync/ZwiftHealthSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZwiftHealthSyncApp.swift
+//  ZwiftHealthSync
+//
+//  Created by Ian Robinson on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct ZwiftHealthSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
